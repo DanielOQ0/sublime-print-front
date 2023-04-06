e586444de078bcf517f871e0dd102eb05ef3cf17
greta <greta.pereda@gmail.com>
2023-04-06 11:15:53 -0600

    footer

--- src/Layouts/MainLayoutUser/MainLayout.jsx
+++ src/Layouts/MainLayoutUser/MainLayout.jsx
@@ -6,7 +6,7 @@ import Footer from '../../Components/Footer/Footer'
 export default function () {
   return (
     <div className='MainLayout'>
-      <p>main layout USER</p>
+        
         <Outlet className="main"/>
         <Footer className="footer"/>
     </div>
