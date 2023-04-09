4a3b994026c2ac478437ab0b003ff64488582ba7
PeredaG <70949726+peredag@users.noreply.github.com>
2023-04-09 07:47:02 -0600

    Revert "G02 (#19)"
    This reverts commit d1038bff652987152c3172ca110df0887c55403d.

--- src/Store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import detailsReducer from './Details/reducer'
-
-export const store = configureStore({
-  reducer: {
-    details:detailsReducer,
-  },
-});
