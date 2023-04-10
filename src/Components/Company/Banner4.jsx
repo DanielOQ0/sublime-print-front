6183178455a9123fbe9786a3add7e8ce2044b074
greta <greta.pereda@gmail.com>
2023-04-09 23:30:04 -0600

    footer, company

---
+++ src/Components/Company/Banner4.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './banner4.css'
+
+export default function Banner4() {
+  return (
+    <div class="container-fluid4 intro4" >
+        <div class="container4">
+            <p>
+            WARRANTY 1 YEAR Some garments are used every day, and washed many times a month, imagine how many times a year! That is why we offer a one-year guarantee: we provide the best quality and you can use it with complete peace of mind.
+            </p>
+        </div>
+    </div>
+  )
+}
