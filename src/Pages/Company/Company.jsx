498391a5019b06c2fbaabd16289dd33da9f23bf2
DanielOQ0 <93298398+DanielOQ0@users.noreply.github.com>
2023-04-10 00:17:33 -0500

    G03 fix (#26)
    * Nav bar con menu hamburguesa
    
    * animaciones
    
    * Vista Home 80%
    
    * store and details
    
    * R05
    
    * correciones
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>

--- src/Pages/Company/Company.jsx
+++ src/Pages/Company/Company.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Banner1 from '../../Components/Company/Banner1'
-import Banner2 from '../../Components/Company/Banner2'
-import Banner3 from '../../Components/Company/Banner3'
-import Banner4 from '../../Components/Company/Banner4'
-import Banner5 from '../../Components/Company/Banner5'
-import Banner6 from '../../Components/Company/Banner6'
+import Banner1 from '../../Components/BannerCompany1/Banner1'
+import Banner2 from '../../Components/BannerCompany1/Banner2'
+import Banner3 from '../../Components/BannerCompany1/Banner3'
+import Banner4 from '../../Components/BannerCompany1/Banner4'
+//import Banner5 from '../../Components/BannerCompany/Banner5'
 import Footer from '../../Components/Footer/Footer'
-
 
 export default function Company() {
   return (
@@ -15,9 +13,10 @@ export default function Company() {
         <Banner2 />
         <Banner3 />
         <Banner4 />
-        <Banner5 />
-        <Banner6 />
+        {/* <Banner5 /> */}
         <Footer />
+       
+       
     </div>
   )
 }
