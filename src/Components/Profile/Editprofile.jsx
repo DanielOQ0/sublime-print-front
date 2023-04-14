191e2ab2b9e672c3cdd2668efaeddb950134564e
PeredaG <70949726+peredag@users.noreply.github.com>
2023-04-14 10:46:27 -0600

    G6 (#57)
    * G02 (#19)
    
    * rutas configuradas y layouts
    
    * Nav bar con menu hamburguesa
    
    * animaciones
    
    * R02 (#11)
    
    * Nav bar con menu hamburguesa
    
    * animaciones
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    
    * footer
    
    * footer
    
    * detalle de producto, footer, carousel, faltan mejoras
    
    * Vista Home 80%
    
    * prueba
    
    * R03 (#17)
    
    * Nav bar con menu hamburguesa
    
    * animaciones
    
    * Vista Home 80%
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    
    * R04 (#18)
    
    * Nav bar con menu hamburguesa
    
    * animaciones
    
    * Vista Home 80%
    
    * store and details
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    
    * x
    
    * arreglos
    
    * company
    
    * register y login provisorios
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    Co-authored-by: DanielOQ0 <93298398+DanielOQ0@users.noreply.github.com>
    
    * Revert "G02 (#19)" (#21)
    
    This reverts commit d1038bff652987152c3172ca110df0887c55403d.
    
    * profile, edit profile, modificaciones en contact
    
    * profile pendiente
    
    * ver
    
    * ver
    
    * profile
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    Co-authored-by: DanielOQ0 <93298398+DanielOQ0@users.noreply.github.com>

--- src/Components/Profile/Editprofile.jsx
+++ src/Components/Profile/Editprofile.jsx
@@ -1,57 +1,83 @@
 import React from 'react'
 import './editprofile.css'
+import { useRef, useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Editprofile() {
+    let token = localStorage.getItem("token")
+    let headers = { headers: { Authorization: `Bearer ${token}` } };
+
+    const editForm = useRef(); 
+    let [user, setUser] = useState({});
+
+    useEffect(() => { 
+        axios.get('http://localhost:8080/api/users/me', headers)
+        .then(res => { 
+            setUser(res.data.user); 
+        });
+        }, 
+        []
+    );
+
+   
+    const handleAccept = async (event) => {
+        event.preventDefault();
+        const data = {
+            photo: editForm.current[0].value,
+            name: editForm.current[2].value,
+        }
+        console.log(data)
+    }
+
   return (
     <div className='profile'>
-     <div class="form-container">
-        <form action="">
+     <div className="form-container">
+        <form ref={editForm}>
 
-           <div class="profile-box">
-               <h4 class="profile-subtitle">Edit Profile</h4>
-               <div class="avatar"></div>
-               <div class="input-file-container">
+           <div className="profile-box">
+               <h4 className="profile-subtitle">Edit Profile</h4>
+               <div className="avatar"><img  className="img-photo-edit" src={user.photo} alt="" /></div>
+               <div className="input-file-container">
                     <input type="file" name="avatar" id=""/>
                </div>
            </div>
 
            
-           <div class="profile-form">
-                <div class="profile-box">
-                    <h4 class="profile-subtitle">Personal information</h4>
-                    <div class="input-container">
-                        <input class="input-style" type="email" name="email" placeholder="Ingrese su email" value="user.email@mail.com" readonly/>
+           <div className="profile-form">
+                <div className="profile-box">
+                    <h4 className="profile-subtitle">Personal information</h4>
+                    <div className="input-container">
+                        <input className="input-style" type="email" name="email" placeholder="Ingrese su email" defaultValue={user.email} readonly/>
                     </div>
-                    <div class="input-container">
-                        <input class="input-style" type="text" name="name" placeholder="Name"/>
-    
+                    <div className="input-container">
+                        <input className="input-style" type="text" name="name" placeholder="Name" defaultValue={user.name}/>
                     </div>
-                    <div class="input-container">
-                        <input class="input-style" type="text" name="lastname" placeholder="Last name"/>
-                    </div>
-                    <div class="input-container">
-                        <input class="input-style" type="number" name="dni" placeholder="DNI"/>
-                    </div>
+                    {/* <div className="input-container">
+                        <input className="input-style" type="text" name="lastname" placeholder="Last name"/>
+                    </div> */}
+                    {/* <div className="input-container">
+                        <input className="input-style" type="number" name="dni" placeholder="DNI"/>
+                    </div> */}
                 </div>
-                <div class="profile-box">
-                    <h4 class="profile-subtitle">Contact information</h4>
-                        <div class="input-container">
-                            <input class="input-style" type="tel" name="tel" placeholder="phone"/>
+                {/* <div className="profile-box">
+                    <h4 className="profile-subtitle">Contact information</h4>
+                        <div className="input-container">
+                            <input className="input-style" type="tel" name="tel" placeholder="phone"/>
                         </div>
-                        <div class="input-container">
-                            <input class="input-style" type="tel" name="tel" placeholder="phone"/>
+                        <div className="input-container">
+                            <input className="input-style" type="tel" name="tel" placeholder="phone"/>
                         </div>
-                        <div class="input-container">
-                            <input class="input-style" type="tel" name="tel" placeholder="phone"/>
+                        <div className="input-container">
+                            <input className="input-style" type="tel" name="tel" placeholder="phone"/>
                         </div>
                       
-                        <div class="input-container">
-                            <input class="input-style" type="text" name="domicilio" placeholder="Street and number"/>
+                        <div className="input-container">
+                            <input className="input-style" type="text" name="domicilio" placeholder="Street and number"/>
                         </div>
-                </div>
+                </div> */}
            </div>
-            <div class="button-container">
-                <button type="submit" class="btn btn-edit">Send</button>
+            <div className="button-container">
+                <button type="submit" className="btn btn-edit" onClick={handleAccept} >Send</button>
             </div> 
         </form>
     </div>
--- src/Store/CaptureUser/actions.js
+++ src/Store/CaptureUser/actions.js
@@ -17,6 +17,24 @@ async()=>{
     }
     
 })
-const actions = { captureUser }
+
+let updateUser = createAsyncThunk("updateUser",
+async()=>{
+    let token = localStorage.getItem("token");
+    let headers = { headers: { Authorization: `Bearer ${token}` } };
+    let url = "http://localhost:8080/api/users/me";
+     try {
+        if(token){
+            let res = await axios.get(url, headers);
+            return { user: res.data.user}
+        }else{return {user:[]}}
+        
+    } catch (error) {
+        return {user:[]}
+    }
+}
+
+) 
+const actions = { captureUser, updateUser  }
 
 export default actions 
