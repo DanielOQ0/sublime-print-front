3061b09b3a11f791f3e74aa44b1b7c47be780907
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2023-04-18 08:14:49 -0500

    Elias (#71)
    * G02 (#19)
    
    * rutas configuradas y layouts
    
    * Nav bar con menu hamburguesa
    
    * animaciones
    
    * R02 (#11)
    
    * Nav bar con menu hamburguesa
    
    * animaciones
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    
    * footer
    
    * footer
    
    * detalle de producto, footer, carousel, faltan mejoras
    
    * Vista Home 80%
    
    * prueba
    
    * R03 (#17)
    
    * Nav bar con menu hamburguesa
    
    * animaciones
    
    * Vista Home 80%
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    
    * R04 (#18)
    
    * Nav bar con menu hamburguesa
    
    * animaciones
    
    * Vista Home 80%
    
    * store and details
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    
    * x
    
    * arreglos
    
    * company
    
    * register y login provisorios
    
    ---------
    
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    Co-authored-by: DanielOQ0 <93298398+DanielOQ0@users.noreply.github.com>
    
    * Revert "G02 (#19)" (#21)
    
    This reverts commit d1038bff652987152c3172ca110df0887c55403d.
    
    * inicio vista de address
    
    * product form, solo falta sizes y colors
    
    * fixed formulario de nuevos productos, select nuevos
    
    * vista de perfil actualizada
    
    * cambios del perfil de usuario para la presentacion
    
    ---------
    
    Co-authored-by: PeredaG <70949726+peredag@users.noreply.github.com>
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>
    Co-authored-by: DanielOQ0 <93298398+DanielOQ0@users.noreply.github.com>

--- src/Components/OrdersContain/OrdersContain.jsx
+++ src/Components/OrdersContain/OrdersContain.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './orderscontain.css'
+import OrderCard from '../OrderCard/OrderCard'
 
 export default function OrdersContain() {
     return (
         <div className="container-orders  bg-blueGray-200 container mx-auto  border-b border-gray-900/10  bg-blueGray-200 bg-white shadow-xl rounded-lg">
             <h1> ORDERS </h1>
+            <OrderCard />
         </div>
     )
 }
